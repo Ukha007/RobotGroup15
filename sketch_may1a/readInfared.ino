f5e44e0fb8434065c314710ebf07d171bc0b6c80
Ukha007 <1khanumar78690@gmail.com>
2024-05-03 08:40:31 -0700

    i did the color sensor

--- sketch_may1a/readInfared.ino
+++ sketch_may1a/readInfared.ino
@@ -1,3 +1,17 @@
-void readInfared() {
-  // Read analog values from 
-}
+void readInfrared() {
+  // Read analog values from infared sensors
+int IRvalue1 = digitalRead(IR_1);
+int IRvalue2 = digitalRead(IR_2);
+int IRvalue3 = digitalRead(IR_3);
+int IRvalue4 = digitalRead(IR_4);
+
+// Print the read values to the serial monitor
+Serial.print("IR_1: ");
+Serial.print(IRvalue1);
+Serial.print(", IR_2: ");
+Serial.print(IRvalue2);
+Serial.print(", IR_3: ");
+Serial.print(IRvalue3);
+Serial.print(", IR_4: ");
+Serial.println(IRvalue4);
+} 
--- sketch_may1a/sketch_may1a.ino
+++ sketch_may1a/sketch_may1a.ino
@@ -1,22 +1,52 @@
-// Reading the Infared Sensors
+// Reading the Infrared Sensors
 #define IR_1 2
 #define IR_2 4
 #define IR_3 7
 #define IR_4 8
 
+// Timer to track the last report of the IR sensors
+unsigned long irSensorMillis = 0;
+
+// Global variable to track the reporting period of the color sensor
+unsigned long colorSensorMillis = 0; 
+
+// TSC3200 library
+#include <tcs3200.h>
+
+// Pin definitions
+#define COLOR_0 A0
+#define COLOR_1 A1
+#define COLOR_2 A2
+#define COLOR_3 A3
+#define COLOR_OUT A4
 
 void setup() {
-  //m put your setup code here, to run once:
-// Setup Infared Pins
+// put your setup code here, to run once:
+
+//Serial begin
+Serial.begin(9600);
+
+// Setup Infrared Pins
 pinMode(IR_1, INPUT);
 pinMode(IR_2, INPUT);
 pinMode(IR_3, INPUT);
 pinMode(IR_4, INPUT);
-
-
 }
-void readInf
-void loop() {
-  // put your main code here, to run repeatedly:
 
+void loop() {
+// put your main code here, to run repeatedly:
+// Get the current run time in milliseconds
+unsigned long currentMillis = millis();
+
+// Check the states of the IR sensors every 500ms / 0.5 seconds
+if (currentMillis - irSensorMillis >= 500) {
+  irSensorMillis = currentMillis;
+  readInfrared();
+}
+
+// Read the color sensor
+if (currentMillis - colorSensorMillis >= 250) {
+  colorSensorMillis = currentMillis;
+  readColorSensor();
+}
 }
